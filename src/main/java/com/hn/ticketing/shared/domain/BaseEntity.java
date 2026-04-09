c19af0e55b6a2a503b5d2ffa776d05c71f0ea44f
hi4579675 <hi4579675@gmail.com>
2026-04-09 15:01:19 +0900

    feat(shared): JPA Auditing 설정 및 BaseEntity 추가

---
+++ src/main/java/com/hn/ticketing/shared/domain/BaseEntity.java
@@ -0,0 +1,27 @@
+package com.hn.ticketing.shared.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    // 이 세 개는 모든 엔티티에 중복 -> 추상 클래스 하나로 상속받아 제거
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+}
